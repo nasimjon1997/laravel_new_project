9c2bbaf187fdb7cf347c66c5c6787b7fbcccfcde
Gurgen <1995mkrtchian@mail.ru>
2023-08-01 15:49:51 +0400

    test

--- database/migrations/2023_08_01_104911_create_ft-migrations.php
+++ database/migrations/2023_08_01_104911_create_ft-migrations.php
@@ -9,34 +9,34 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function stringup(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->string('id',20)->primary()->nullable();
-            $table->char('firstname', 256)->nullable();
-            $table->char('lastname', 256)->nullable();
-            $table->char('email', 256)->unique()->nullable();
-            $table->char('role', 256)->nullable();
-            $table->char('status', 20)->nullable();
+            $table->string('firstname', 256)->nullable();
+            $table->string('lastname', 256)->nullable();
+            $table->string('email', 256)->unique()->nullable();
+            $table->string('role', 256)->nullable();
+            $table->string('status', 20)->nullable();
             $table->dateTime('registered')->nullable();
-            $table->char('password', 256)->nullable();
+            $table->string('password', 256)->nullable();
         });
 
         Schema::create('categories', function (Blueprint $table) {
             $table->string('id',20)->primary()->nullable();
-            $table->char('title', 256)->nullable();
+            $table->string('title', 256)->nullable();
             $table->longText('description')->nullable();
-            $table->char('slug', 256)->nullable();
+            $table->string('slug', 256)->nullable();
         });
 
         Schema::create('news', function (Blueprint $table) {
             $table->string('id',20)->primary()->nullable();
-            $table->char('title', 256)->nullable();
+            $table->string('title', 256)->nullable();
             $table->longText('content')->nullable();
-            $table->char('slug', 256)->nullable();
+            $table->string('slug', 256)->nullable();
             $table->integer('cid')->nullable();
             $table->integer('uid')->nullable();
-            $table->char('status', 20)->nullable();
+            $table->string('status', 20)->nullable();
             $table->dateTime('created')->nullable();
             $table->dateTime('modified')->nullable();
 
